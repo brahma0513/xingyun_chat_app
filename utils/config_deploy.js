55f4b734a106c4082a9e475fe8051d8bd74d01bb
brahma0513 <44428971+brahma0513@users.noreply.github.com>
2026-09-10 18:50:31 +0800

    chore: track app deployment configuration

---
+++ utils/config_deploy.js
@@ -0,0 +1,166 @@
+// export const apiUrl = 'https://ypt.yungouzhenpin.com'
+// export const customer_id = 6528
+// export const customer_id_en = 'czo0OiI2NTI4Ijs' 
+// export const api_key = '96d65ac10ad36205df03541795ef7ed2'
+
+// export const apiUrl = 'https://ypt.beizhoudao.com'
+// export const customer_id = 4193
+// export const customer_id_en = 'czo0OiI0MTkzIjs' 
+// export const api_key = '3ac76efea541b102b04c3d0862399d74'
+
+// export const apiUrl = 'https://oemkangshifu.st.wsy010.cn'
+// export const customer_id = 5327
+// export const customer_id_en = 'czo0OiI1MzI3Ijs'
+// export const api_key = '3ac76efea541b102b04c3d0862399d74'
+
+// export const apiUrl = 'https://ypt.hapinlegou.com'
+// export const customer_id = 7006 
+// export const customer_id_en = 'czo0OiI3MDA2Ijs' 
+// export const api_key = 'eb42e50494b96893f8fb0c30efb72535'
+
+// export const apiUrl = 'https://ypt.sml123.com'
+// export const customer_id = 9879
+// export const customer_id_en = 'czo0OiI5ODc5Ijs'
+// export const api_key = '493bf70fe2898ab089faa336e6e71a3d' 
+
+// export const apiUrl = 'https://ypt.zhuyouwei.cn'
+// export const customer_id = 6969
+// export const customer_id_en = 'czo0OiI2OTY5Ijs' 
+// export const api_key = '9a82c1080864d40b588d0e5ba618d923'
+
+// export const apiUrl = 'https://ypt.galaxysharing.com'
+// export const cidUrl = 'https://apitest.galaxysharing.com'
+// export const customer_id = 9307
+// export const customer_id_en = 'czo0OiI5MzA3Ijs' 
+// export const api_key = '2bc0fff9b1578a07559e9c9b15a6d555'
+
+// export const apiUrl = 'https://cloud.ynyckeji.com'
+// export const customer_id = 9569
+// export const customer_id_en = 'czo0OiI5NTY5Ijs' 
+// export const api_key = 'mVP1pbZdoLXVPDkK2LSstKGzSUoVZAPektw19DP4'
+
+// export const apiUrl = 'https://ypt.senlinmaoyi.com'
+// export const customer_id = 5941
+// export const customer_id_en = 'czo0OiI1OTQxIjs' 
+// export const api_key = 'b7e91e6973c9fc8e3e010c011e02c3c5'
+
+// export const apiUrl = 'https://develop.dongguanhongyun.com'
+// export const customer_id = 5901  
+// export const customer_id_en = 'czo0OiI1OTAxIjs' 
+// export const api_key = 'dev'
+
+// export const apiUrl = 'https://admin.88588.net.cn'
+// export const customer_id = 8219  
+// export const customer_id_en = 'czo0OiI4MjE5Ijs' 
+// export const api_key = 'yiyun'
+
+
+// export const apiUrl = 'https://chanqi.youmishenghuo.com.cn'
+// export const customer_id = 6224
+// export const customer_id_en = 'czo0OiI2MjI0Ijs' 
+// export const api_key = 'e16a9be3d5ceec1c22716810760c641a'
+
+// export const apiUrl = 'https://ypt.bainonghui.com.cn'
+// export const customer_id = 3233 
+// export const customer_id_en = 'czo0OiIzMjMzIjs' 
+// export const api_key = 'q9wHBzmYbRL6Rp3BbdVNSSJXhBK9Vyprh3cVppn2'
+
+// export const apiUrl = 'https://ypt.lizhonglianying.com'
+// export const customer_id = 7667 
+// export const customer_id_en = 'czo0OiI3NjY3Ijs' 
+// export const api_key = 'iR6sYP6cZVh3hnXF4rHwjxhg1W5tX3GfTYaHBcpX' 
+
+
+// export const apiUrl = 'https://ypt.yuezhishang.com'
+// export const customer_id = 6792
+// export const customer_id_en = 'czo0OiI2NzkyIjs' 
+// export const api_key = 'UmLxK23PjUYvR1YmwA7Ff7LnvWtXVfERQ4fzKD8u'
+
+// export const apiUrl = 'https://ypt.jiliu913.com'
+// export const customer_id = 3632 
+// export const customer_id_en = 'czo0OiIzNjMyIjs' 
+// export const api_key = '14dd8a29d9ec9306f59dbdc738d04e5e'
+
+// export const apiUrl = 'https://yun.new.xingdian666.com'
+// export const customer_id = 2842
+// export const customer_id_en = 'czo0OiIyODQyIjs' 
+// export const api_key = 'xingdian'
+
+// export const apiUrl = 'https://ypt.gd-umiga.com'
+// export const customer_id = 7474
+// export const customer_id_en = 'czo0OiI3NDc0Ijs ' 
+// export const api_key = 'e23d689c24a0ce70e37390211e25c79d'
+
+// export const apiUrl = 'https://ypt.jiajiayoushop.com'
+// export const customer_id = 6430
+// export const customer_id_en = 'czo0OiI2NDMwIjs' 
+// export const api_key = 'H4eVCxafP4YVvVU2j57NEbXevNKUjuBV8F2fvaMs'
+
+// export const apiUrl = 'https://ypt.yxbl168.com'
+// export const customer_id = 2539
+// export const customer_id_en = 'czo0OiIyNTM5Ijs' 
+// export const api_key = '8ZRxWtec7cfFKRqKtHvb8rLr8cEgC71wHaFkfCg8'
+
+// export const apiUrl = 'https://cloud.facenow.vip'
+// export const customer_id = 6246
+// export const customer_id_en = 'czo0OiI2MjQ2Ijs' 
+// export const api_key = 'JQFxSyx77QU78vY6nTGHqgbuHFnyF9MqfTSGFzyw'
+
+// export const apiUrl = 'https://ypt.bainonghui.com.cn'
+// export const customer_id = 3233  
+// export const customer_id_en = 'czo0OiIzMjMzIjs' 
+// export const api_key = 'q9wHBzmYbRL6Rp3BbdVNSSJXhBK9Vyprh3cVppn2'
+
+// export const apiUrl = 'https://ypt.kangaishanyuan.com'
+// export const customer_id = 8704
+// export const customer_id_en = 'czo0OiI4NzA0Ijs' 
+// export const api_key = 'bqVKG9iZQ7p7axsGbfWNW7T6zubu3VVwgyowBt78'
+
+// export const apiUrl = 'https://ypt.lezhuankeji.com'
+// export const customer_id = 1507
+// export const customer_id_en = 'czo0OiIxNTA3Ijs' 
+// export const api_key = 'aoeCKTVsTSZofPwczLCKvXyJ7xFScbwrXxvgm2XU'
+
+// export const apiUrl = 'https://cloud.facenow.vip'
+// export const customer_id = 6246
+// export const customer_id_en = 'czo0OiI2MjQ2Ijs' 
+// export const api_key = 'JQFxSyx77QU78vY6nTGHqgbuHFnyF9MqfTSGFzyw'
+
+// export const apiUrl = 'https://ypt.zxyg66.com'
+// export const customer_id = 1913
+// export const customer_id_en = ' czo0OiIxOTEzIjs' 
+// export const api_key = 'yenL5Qa5gZpGN9uLd1PxgikeRzPM82YvjWXg3moD'
+
+// export const apiUrl = 'https://cloud.yueyuebao.net'
+// export const customer_id = 6057
+// export const customer_id_en = 'czo0OiI2MDU3Ijs' 
+// export const api_key = '49WF88muoASwdtNxLQHWwnKxSXcHpLegkZiqZBaw'
+
+// export const apiUrl = 'https://ypt.bazhaoyu.net.cn'
+// // export const apiUrl = 'https://yun.new.xingdian666.com/'
+// export const customer_id = 9599 
+// export const customer_id_en = 'czo0OiI5NTk5Ijs'
+// export const api_key = 'M9fePG692evt3VUq'
+
+// export const apiUrl = 'https://ypt.hnjcsh.com/'
+// // export const apiUrl = 'https://yun.new.xingdian666.com/'
+// export const customer_id = 5696
+// export const customer_id_en = 'czo0OiI1Njk2Ijs'
+// export const api_key = 'JQCQJ425SS8LbFFvnxG3856bBWueGac4Ybu4PLaT'
+
+// export const apiUrl = 'https://ypt.hnjcsh.com/'
+export const apiUrl = 'https://yun.new.xingdian666.com'
+// export const apiUrl = 'https://yun.admin.com'
+export const customer_id = 2842
+export const customer_id_en = 'czo00ilyODQyljs'
+export const api_key = 'xingdian'
+
+
+export const appExamine = false;	//当前是否审核的版本  true是审核中  false是正式 
+
+//是否H5打包
+export const is_h5_package = false; 
+
+// // app版本
+export const appVersion = '3.0.10'
+//HBuilder X 版本:3.4.6
